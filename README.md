cc5a5151d6db5b0ca15fc40deb01410ab65d7037
ArturStahov <vtemy.at.ua@gmail.com>
2020-12-05 19:01:50 +0200

    add readme.md

--- README.md
+++ README.md
@@ -5,3 +5,11 @@ use libs:
 - axios;
 - styled-components;
 - csstools/normalize;
+- react-three-fiber;
+- drei;
+- react-spring/three;
+- pnotify;
+- Rest full Api:
+  https://developers.themoviedb.org/3/getting-started/introduction
+
+### netlify hosting https://rokkart-filmsinfo-app.netlify.app
--- src/components/AnimationBackground/AnimationBackground.js
+++ src/components/AnimationBackground/AnimationBackground.js
@@ -16,22 +16,14 @@ const SpinningMesh = ({ position, color, speed, args }) => {
   //useFrame allows us to re-render/update rotation on each frame
   useFrame(() => (mesh.current.rotation.x = mesh.current.rotation.y += 0.01));
   //Basic expand state
-  const [expand, setExpand] = useState(false);
   const [hovered, setHover] = useState(false);
-
-  // React spring expand animation
-  const props = useSpring({
-    scale: expand ? [1.4, 1.4, 1.4] : [1, 1, 1],
-  });
 
   return (
     <a.mesh
       position={position}
       ref={mesh}
-      onClick={() => setExpand(!expand)}
       onPointerOver={event => setHover(true)}
       onPointerOut={event => setHover(false)}
-      scale={props.scale}
       castShadow
     >
       <boxBufferGeometry attach="geometry" args={args} />
